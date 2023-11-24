c1920bf274321b83dbe9814cfcf4cccc8ae6067b
Shawn Logan <sml@sml.fios-router.home>
2023-11-24 17:00:37 -0500

    1. Added new shell script install_symbolic_link.sh to create symbolic link for vpulse in $HOME/bin directory if direcory exists. Script install_symbolic_link.sh removes any file with name vpulse in $HOME/bin directory and re-creates a symbolic link to vpulse in this src directory
    2. Updated makefile to include call to shell script install_symbolic_link.sh
    
    3. Updated version and date in vpulse.h to v2.02 and 11/24/2023

---
+++ src/install_symbolic_link.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if test -d $HOME/bin; then
+	if test -f $HOME/bin/vpulse; then
+		# echo "File vpulse exists...deleting file to replace it with new version."
+		rm $HOME/bin/vpulse
+	fi
+	ln -s $PWD/vpulse $HOME/bin/vpulse
+	# echo "File vpulse created..."
+	# ls -l $HOME/bin/vpulse
+fi
